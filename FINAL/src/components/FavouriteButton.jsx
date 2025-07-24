f9fbee80bb7ef2405ebed4adc4c0da7be51821f3
asanda9999 <asandamkhize9@gmail.com>
2025-07-24 12:21:39 +0200

    Added Favourite button

---
+++ FINAL/src/components/FavouriteButton.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { isFavourite, toggleFavourite } from "../utils/favourites";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+export default function FavouriteButton({ episodeKey }) {
+  const [favourite, setFavourite] = useState(isFavourite(episodeKey));
+
+  useEffect(() => {
+    setFavourite(isFavourite(episodeKey));
+  }, [episodeKey]);
+
+  const handleToggle = () => {
+    toggleFavourite(episodeKey);
+    setFavourite(isFavourite(episodeKey));
+  };
+
+  return (
+    <button
+      onClick={handleToggle}
+      style={{
+        marginLeft: 12,
+        background: "none",
+        border: "none",
+        cursor: "pointer",
+        fontSize: 22,
+        display: "flex",
+        alignItems: "center"
+      }}
+      aria-label="Toggle Favourite"
+    >
+      {favourite ? <AiFillHeart color="#e25555" /> : <AiOutlineHeart color="#bbb" />}
+    </button>
+  );
+} 
